14901012efcbeb7cf21836462c5b82ca6ba9282c
Md. Asif Aman Apon <aponasifaman@gmail.com>
2023-03-21 21:08:21 +0600

    mext-app

--- laravel-basic-website/resources/views/admin/body/header.blade.php
+++ laravel-basic-website/resources/views/admin/body/header.blade.php
@@ -14,15 +14,15 @@
 
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm-light" height="120">
+                        <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="logo-sm-light" height="80">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="logo-light" height="120">
+                        <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="logo-light" height="80">
                     </span>
                 </a>
             </div>
 
-            <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect" id="vertical-menu-btn">
+            <button type="button" class="btn btn-sm px-3 font-size-24 header-item waves-effect pl-12" id="vertical-menu-btn" style="margin-left:5rem">
                 <i class="ri-menu-2-line align-middle"></i>
             </button>
 
